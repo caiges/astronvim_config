f14fc2c4a9c2ed2da836de31a1e98381ce66f6db
caiges <caigesn@gmail.com>
2023-08-07 16:14:43 -0700

    Add terraform plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -8,4 +8,5 @@ return {
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.terraform" },
 }
